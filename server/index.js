3b0c0436a7def5e69be5bc00cf9b88c9a3729ebe
HunngryMan <angel.b.sosa@outlook.com>
2023-12-27 17:49:56 -0600

    basic server and app

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// server.index.js
+
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Hacer que node sirva los archivos de nuestro app React
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+    res.json({message: "Hola desde el servidor!"});
+});
+
+// Todas las peticiones GET que no hayamos manejado en
+// las lineas anteriores retornaran nuestra app React
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
